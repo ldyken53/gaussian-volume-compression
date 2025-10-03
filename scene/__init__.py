a65c2150059f5f4d67bd6eff9379be9690e65205
Landon Dyken <ldyken@lanl.gov>
2025-10-03 12:10:37 -0600

    Change back to structured

--- scene/__init__.py
+++ scene/__init__.py
@@ -33,7 +33,7 @@ class Scene:
 
         if os.path.exists(args.source_path) and args.source_path.lower().endswith(('.vtk', '.vtu')):
             # mesh, pcd = readData(args.source_path, fraction, normalized=normalized)
-            mesh, pcd = readDatau(args.source_path, fraction)
+            mesh, pcd = readData(args.source_path, fraction)
         else:
             assert False, "Could not recognize scene type!"
 
